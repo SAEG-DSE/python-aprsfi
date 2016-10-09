6e218081ca06887529f96f0cae86c32c3d958161
whanderley <whanderley.souza@iff.edu.br>
2016-10-09 12:44:26 -0300

    request for one name return locations attributes

--- api.py
+++ api.py
@@ -1,8 +1,15 @@
-import string
-
+import json
+import requests
+from aprsfiapi.response import Response
 
 class API(object):
 
     def __init__(self, api_key):
         self._api_key = api_key
-        self._api_url = "http://api.aprs.fi/api/get?name={name}&what={what}&apikey=%s&format=json" % api_key
+        self._api_url = ("http://api.aprs.fi/api/get?name={name}&what={what}&"
+                         "apikey=%s&format=json" % api_key)
+
+    def loc(self, *args):
+        url = self._api_url.format(name=','.join(args), what='loc')
+        response = requests.get(url)
+        return Response(json.loads(response.json()))
--- tests/aprsfiapi_spec.py
+++ tests/aprsfiapi_spec.py
@@ -1,5 +1,9 @@
 import unittest
+import requests_mock
+import json
 from aprsfiapi.api import API
+from aprsfiapi.response import Response
+from aprsfiapi.tests.support import requests
 
 
 class APITest(unittest.TestCase):
@@ -12,4 +16,9 @@ class APITest(unittest.TestCase):
     def test_initialize_with_string_template_for_aprs_fi_url(self):
         self.assertEqual(self.aprsfiapi._api_url, "http://api.aprs.fi/api/get?name={name}&what={what}&apikey=api_key_example&format=json")
 
-    # def test_return_basic_location_json_for_a_single_name(self):
+    @requests_mock.mock()
+    def test_return_basic_location_json_for_a_single_name(self, m):
+        m.get('http://api.aprs.fi/api/get?name=OH7RDA&what=loc&apikey=api_key_example&format=json',
+            json=json.dumps(requests.single_name_location_success(), ensure_ascii=False))
+        response  = Response(requests.single_name_location_success())
+        self.assertEqual(self.aprsfiapi.loc('OH7RDA'), response)
