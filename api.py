4cc40a96ad3c38bf1b7e181993d515029c34ced1
whanderley <whanderley.souza@iff.edu.br>
2016-10-09 18:03:10 -0300

    refactory api with metaprogramming

--- api.py
+++ api.py
@@ -14,16 +14,14 @@ class API(object):
         self._api_url = ("http://api.aprs.fi/api/get?name={name}&what={what}&"
                          "apikey=%s&format=json" % api_key)
 
-    def loc(self, *args):
-        url = self._api_url.format(name=','.join(args), what='loc')
-        response = requests.get(url)
-        if json.loads(response.json())['result'] == 'fail':
-            raise RequestException(json.loads(response.json())['description'])
-        return Response(json.loads(response.json()))
-
-    def wx(self, *args):
-        url = self._api_url.format(name=','.join(args), what='wx')
-        response = requests.get(url)
-        if json.loads(response.json())['result'] == 'fail':
-            raise RequestException(json.loads(response.json())['description'])
-        return Response(json.loads(response.json()))
+    def __getattr__(self, attr):
+        if attr in ['loc', 'wx']:
+            def request(*args):
+                url = self._api_url.format(name=','.join(args), what=attr)
+                response = requests.get(url)
+                if json.loads(response.json())['result'] == 'fail':
+                    raise RequestException(json.loads(response.json())['description'])
+                return Response(json.loads(response.json()))
+            return request
+        else:
+            raise AttributeError
