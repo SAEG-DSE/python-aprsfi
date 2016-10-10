27d81b6de4bf607c4f8fbc5e77aeda7f4f645bdf
whanderley <whanderley.souza@iff.edu.br>
2016-10-10 14:19:14 -0300

    remove json from setup

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ except ImportError:
 
 setup(
     name='aprsfi',
-    version='0.1.1',
+    version='0.1.2',
     license='LGPLv3',
     description='python client aprs.fi API.',
     long_description='A complete Python client for aprs.fi',
     packages=['aprsfi'],
     author="Whanderley Souza Freitas",
     author_email='whanderley.souza@iff.edu.br',
-    install_requires=['requests', 'json'],
+    install_requires=['requests'],
     url='https://github.com/SAEG-DSE/aprsfi',
-    download_url='https://github.com/SAEG-DSE/python-aprsfi/archive/0.1.1.tar.gz',
+    download_url='https://github.com/SAEG-DSE/python-aprsfi/archive/0.1.2.tar.gz',
     keywords='aprsfi api client wrapper',
 )
